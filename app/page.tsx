e5f4eb696b2303940c6db05b04c4a662923edb8d
Prajwal Deshmukh <smartpd2607@gmail.com>
2023-08-03 16:17:10 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,105 @@
-import Image from 'next/image'
+"use client"
+import { useState } from 'react';
+import Image from 'next/image';
 
 export default function Home() {
+  const [productName, setProductName] = useState('');
+  const [productDescription, setProductDescription] = useState('');
+  const [generatedDescription, setGeneratedDescription] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const handleProductNameChange = (event) => {
+    setProductName(event.target.value);
+  };
+
+  const handleProductDescriptionChange = (event) => {
+    setProductDescription(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    setIsGenerating(true); // Show "Generating..." text on the button
+    try {
+      const API_ENDPOINT = 'https://api.worqhat.com/api/ai/content/v2';
+      const API_KEY = 'U2FsdGVkX187FPQxzgbmIVjXh3O1+xyor30KWVrIBMuFEqGv8NfzXPjE53e3Ju+T';
+      const ORG_KEY = 'U2FsdGVkX19lq3bhhF5TRouMiyL2HvEBD2V5j5nNl6dNL9JWPbsXW0rqlzssW8GieFki6oRVDKTb/z01Hc7m+Q==';
+
+      const question = `Generate the description for product: ${productName}. Description: ${productDescription}`;
+
+      const response = await fetch(API_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': API_KEY,
+          'x-org-key': ORG_KEY,
+        },
+        body: JSON.stringify({
+          question: question,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Error fetching data from API');
+      }
+
+      const data = await response.json();
+      // Assuming the API response contains a 'content' field with the generated description
+      setGeneratedDescription(data.content);
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle error state here
+    } finally {
+      setIsGenerating(false); // Reset the button text back to "Generate Description"
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex flex-col h-full items-center justify-center p-5">
+      <div className="container h-full my-1 bg-white rounded-sm text-white p-10">
+        <h1 className="my-3 font-sans text-center text-4xl font-bold p-4 drop-shadow-lg bg-blue-600">
+          Product Descriptor By Worqhat 🎓
+        </h1>
+        <div className="text-center container rounded-md text-white my-4 p-5 flex flex-col items-center justify-center">
+          <div className="upload flex flex-col items-center">
+            <div>
+              <input
+                type="text"
+                placeholder="Enter Product Name"
+                value={productName}
+                onChange={handleProductNameChange}
+                className="border-2 text-black rounded-md w-96 p-4 m-2 text-center drop-shadow-xl"
+              />
+            </div>
+          </div>
+          <div className="upload flex flex-col items-center">
+            <div>
+              <input
+                type="text"
+                placeholder="Describe your product"
+                value={productDescription}
+                onChange={handleProductDescriptionChange}
+                className="border-2 text-black rounded-md w-96 p-4 m-2 text-center drop-shadow-xl"
+              />
+            </div>
+          </div>
+          <button
+            className={`btn bg-blue-600 text-white p-3 mx-auto cursor-pointer rounded-md ${
+              isGenerating ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
+            onClick={handleSubmit}
+            disabled={isGenerating}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {isGenerating ? 'Generating...' : 'Generate Description'}
+          </button>
+        </div>
+        <div className="cv bg-white h-64 rounded-md text-white p-5">
+          <textarea
+            className="border-2 text-black rounded-md w-full h-64 p-4 text-center drop-shadow-xl"
+            placeholder="Welcome to Product descriptor by Worqhat! Enter a product name to generate the description."
+            value={generatedDescription}
+            readOnly
+          ></textarea>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
